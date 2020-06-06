00dbb81af12b0add4636024e3ccf0a26ec8f0d61
hainv <hai213k57@gmail.com>
2020-06-06 19:38:57 +0700

    Code example demo qboxlayout
    Signed-off-by: hainv <hai213k57@gmail.com>

---
+++ python/qt/qboxlayout.py
@@ -0,0 +1,75 @@
+"""
+QBoxLayout class lines up the widgets vertically or horizontally.
+Its derived classes are QVBoxLayout (for arranging widgets vertically) 
+and QHBoxLayout (for arranging widgets horizontally).
+"""
+
+import sys
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QDialog, QPushButton, QVBoxLayout, QHBoxLayout
+
+
+def vertical():
+    app = QApplication(sys.argv)
+    win = QWidget()
+
+    btn1 = QPushButton('Button 1')
+    btn2 = QPushButton('Button 2')
+
+    vbox = QVBoxLayout()
+    vbox.addStretch()
+    vbox.addWidget(btn1)
+    vbox.addWidget(btn2)
+
+    win.setLayout(vbox)
+    win.setWindowTitle('PyQt')
+    win.show()
+    sys.exit(app.exec_())
+
+
+def horizontal():
+    app = QApplication(sys.argv)
+    win = QWidget()
+
+    btn1 = QPushButton('Button 1')
+    btn2 = QPushButton('Button 2')
+
+    hbox = QHBoxLayout()
+    hbox.addStretch()
+    hbox.addWidget(btn1)
+    hbox.addWidget(btn2)
+    win.setLayout(hbox)
+    win.setWindowTitle('PyQt')
+    win.show()
+    sys.exit(app.exec_())
+
+
+def verticalandhorizontal():
+    app = QApplication(sys.argv)
+    win = QWidget()
+
+    btn1 = QPushButton('Button 1')
+    btn2 = QPushButton('Button 2')
+    vbox = QVBoxLayout()
+    vbox.addStretch()
+    vbox.addWidget(btn1)
+    vbox.addWidget(btn2)
+
+    btn3 = QPushButton('Button 3')
+    btn4 = QPushButton('Button 4')
+    hbox = QHBoxLayout()
+    hbox.addStretch()
+    hbox.addWidget(btn3)
+    hbox.addWidget(btn4)
+
+    vbox.addLayout(hbox)
+    win.setLayout(vbox)
+    win.setWindowTitle('PyQt')
+    win.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    # vertical()
+    # horizontal()
+    verticalandhorizontal()
