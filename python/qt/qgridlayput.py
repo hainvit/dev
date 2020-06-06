41270bac329fe1aca735053b08fc1eabf46f7260
hainv <hai213k57@gmail.com>
2020-06-06 21:21:37 +0700

    Code example demo QGridLayout of PyQt
    Signed-off-by: hainv <hai213k57@gmail.com>

---
+++ python/qt/qgridlayput.py
@@ -0,0 +1,30 @@
+"""
+A GridLayout class object presents with a grid of cells arranged in rows and columns.
+The class contains addWidget() method. Any widget can be added by specifying the number
+of rows and columns of the cell. Optionally, a spanning factor for row as well as column,
+if specified makes the widget wider or taller than one cell.
+"""
+
+import sys
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QDialog, QPushButton, QGridLayout
+
+
+def main():
+    app = QApplication(sys.argv)
+    win = QWidget()
+    grid = QGridLayout()
+
+    for i in range(1, 5):
+        for j in range(1, 5):
+            grid.addWidget(QPushButton('B'+str(i)+str(j)), i, j)
+
+    win.setLayout(grid)
+    win.setGeometry(100, 100, 200, 100)
+    win.setWindowTitle('PyQt')
+    win.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
