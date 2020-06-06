45030d86fdcb04a29f2f917b1b21fa58930425c8
hainv <hai213k57@gmail.com>
2020-06-06 19:00:52 +0700

    Code demo signals/slot of PyQt
    Signed-off-by: hainv <hai213k57@gmail.com>

---
+++ python/qt/signals_and_slot.py
@@ -0,0 +1,40 @@
+"""
+Signals and slot
+"""
+
+import sys
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QDialog, QPushButton
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    win = QDialog()
+
+    btn1 = QPushButton(win)
+    btn1.setText('Button 1')
+    btn1.move(50, 20)
+    btn1.clicked.connect(clicked_btn1)
+
+    btn2 = QPushButton(win)
+    btn2.setText('Button 2')
+    btn2.move(50, 50)
+    btn2.clicked.connect(clicked_btn2)
+
+    win.setGeometry(100, 100, 200, 100)
+    win.setWindowTitle('Signals and slot')
+    win.show()
+    sys.exit(app.exec_())
+
+
+def clicked_btn1():
+    print('Clicked btn1')
+
+
+def clicked_btn2():
+    print('Clicked btn2')
+
+
+if __name__ == '__main__':
+    main()
